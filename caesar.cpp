4ddae50da8fd6e3bb4509d96fa5f5e2ef3baad85
sansona <jiaming.justin.chen@gmail.com>
2017-09-20 12:01:27 -0700

    Fixed indentations

--- caesar.cpp
+++ caesar.cpp
@@ -7,90 +7,90 @@ using namespace std;
 //Caeser cipher
 int main(int argc, char *argv[])
 {
-  if (argc != 2)
-  {
-    cout << "usage: " << argv[0] << " key (integer)\n";
-  }
+    if (argc != 2)
+    {
+        cout << "usage: " << argv[0] << " key (integer)\n";
+    }
 
-  else
-  {
-    string message;
-    int key = atoi(argv[1]);
-    int option;
-    cout << "Choose an option for the Caesar cipher:\n 1) to encrypt \n 2) to decrypt\n";
-    cin >> option;
+    else
+    {
+        string message;
+        int key = atoi(argv[1]);
+        int option;
+        cout << "Choose an option for the Caesar cipher:\n 1) to encrypt \n 2) to decrypt\n";
+        cin >> option;
     
     if (option == 1)
     {
 
-      cout << "\nEnter the message in plaintext\n\n";
-      cin >> message;
+        cout << "\nEnter the message in plaintext\n\n";
+        cin >> message;
 
 
-      for (int i = 0; i < message.length(); i++)
+        for (int i = 0; i < message.length(); i++)
 
-      {
-        if (isalpha(message[i]))
         {
-          if (isupper(message[i]))
-          {
-            char c = message[i];
-            cout << (char)((c - 65 + key) % 26 + 65); 
-          }
+            if (isalpha(message[i]))
+            {
+                if (isupper(message[i]))
+                {
+                    char c = message[i];
+                    cout << (char)((c - 65 + key) % 26 + 65); 
+                }
 
-          if (islower(message[i]))
-          {
-            char c = message[i];
-            cout << char((c - 97 + key) % 26 + 97);
-          } 
+            if (islower(message[i]))
+            {
+                char c = message[i];
+                cout << char((c - 97 + key) % 26 + 97);
+            } 
+            }
+
+            else 
+            {
+            cout << message[i];
+            }
+
         }
 
-        else 
-        {
-          cout << message[i];
-        }
-
-      }
-
-    cout << "\n";
+        cout << "\n";
     }
   
 
-  if (option == 2)
-  {
-    cout << "\nEnter the message in ciphertext\n\n";
-    cin >> message;
-
-
-    for (int i = 0; i < message.length(); i++)
-
+    if (option == 2)
     {
-      if (isalpha(message[i]))
-      {
-         if (isupper(message[i]))
-         {
-            char c = message[i];
-            cout << (char)((c - 65 - key) % 26 + 65); 
-         }
-
-         if (islower(message[i]))
-         {
-            char c = message[i];
-            cout << char((c - 97 - key) % 26 + 97);
-         } 
-      }
-
-      else 
-      {
-        cout << message[i];
-      }
-      }
-
-      cout << "\n";
+        cout << "\nEnter the message in ciphertext\n\n";
+        cin >> message;
 
 
+        for (int i = 0; i < message.length(); i++)
+
+        {
+            if (isalpha(message[i]))
+            {
+                if (isupper(message[i]))
+                {
+                    char c = message[i];
+                    cout << (char)((c - 65 - key) % 26 + 65); 
+                }
+
+                if (islower(message[i]))
+                {
+                    char c = message[i];
+                    cout << char((c - 97 - key) % 26 + 97);
+                } 
+            }
+
+            else 
+            {
+                cout << message[i];
+            }
+        }
+
+        cout << "\n";
 
 
-  }
-}
+
+
+    }
+    }
 }
--- vigenere.cpp
+++ vigenere.cpp
@@ -9,106 +9,99 @@ using namespace std;
 //Vigenere cipher
 int main(int argc, char *argv[])
 {
-  if (argc != 2)
-  {
-    cout << "usage: " << argv[0] << " keyword\n";
-  }
+    if (argc != 2)
+    {
+        cout << "usage: " << argv[0] << " keyword\n";
+    }
 
-  else
-  {
-    string message;
-    vector <int> keyword;
-    //int keyword [strlen(argv[1])];
-    int option;
-    cout << "Choose an option for the Vigenere cipher:\n 1) to encrypt \n 2) to decrypt\n";
-    cin >> option;
+    else
+    {
+        string message;
+        vector <int> keyword;
+
+        int option;
+        cout << "Choose an option for the Vigenere cipher:\n 1) to encrypt \n 2) to decrypt\n";
+        cin >> option;
 
     if (option == 1)
     {
-    
-      cout << "\nEnter the message in plaintext\n\n";
-      cin >> message;
+
+        cout << "\nEnter the message in plaintext\n\n";
+        cin >> message;
  
-      for (int i = 0; i < strlen(argv[1]); i++)
-      {
-        keyword.push_back(toupper(argv[1][i]) - 65); // converts each char of argv[1] to alphanumeric value 
-      } 
+        for (int i = 0; i < strlen(argv[1]); i++)
+        {
+            keyword.push_back(toupper(argv[1][i]) - 65); // converts each char of argv[1] to alphanumeric value 
+        } 
 
         for (int j = 0; j < message.length(); j++)
-
         {
-          if (isalpha(message[j]))
-          {
-            if (isupper(message[j]))
+            if (isalpha(message[j]))
             {
-              char c = message[j];
-              cout << (char)((c - 65 + keyword[j % keyword.size()]) % 26 + 65);
-            }
+                if (isupper(message[j]))
+                {
+                    char c = message[j];
+                    cout << (char)((c - 65 + keyword[j % keyword.size()]) % 26 + 65);
+                }
 
             if (islower(message[j]))
             {
-              char c = message[j];
-              cout << (char)((c - 97 + keyword[j % keyword.size()]) % 26 + 97);
+                char c = message[j];
+                cout << (char)((c - 97 + keyword[j % keyword.size()]) % 26 + 97);
             }
-          }
-        
-        else
-        {
-          cout << message[j];
+            }          
+
+            else
+            {
+                cout << message[j];
+            }
+
         }
 
-      
-    }
-
-    cout << "\n";
+        cout << "\n";
 
     }
 
-   if (option == 2)
-   {
-
-     cout << "\nEnter the message in ciphertext\n\n";
-     cin >> message;
-
-    for (int i = 0; i < strlen(argv[1]); i++)
+    if (option == 2)
     {
-       keyword.push_back(toupper(argv[1][i]) - 65); // converts each char of argv[1] to alphanumeric value 
-    }
 
-      for (int j = 0; j < message.length(); j++)
-      {
-        if (isalpha(message[j]))
-        {
-          if (isupper(message[j]))
-          {
-            char c = message[j];
-            cout << (char)((c - 65 - keyword[j % keyword.size()]) % 26 + 65);
-          }
+        cout << "\nEnter the message in ciphertext\n\n";
+        cin >> message;
 
-          if (islower(message[j]))
-          {
-            char c = message[j];
-            cout << (char)((c - 97 - keyword[j % keyword.size()]) % 26 + 97);
-          }
-        }
-        
-        else
+        for (int i = 0; i < strlen(argv[1]); i++)
         {
-          cout << message[j];
+            keyword.push_back(toupper(argv[1][i]) - 65); // converts each char of argv[1] to alphanumeric value 
         }
 
-      
+        for (int j = 0; j < message.length(); j++)
+        {
+            if (isalpha(message[j]))
+            {
+                if (isupper(message[j]))
+                {
+                    char c = message[j];
+                    cout << (char)((c - 65 - keyword[j % keyword.size()]) % 26 + 65);
+                }
+
+                if (islower(message[j]))
+                {
+                    char c = message[j];
+                    cout << (char)((c - 97 - keyword[j % keyword.size()]) % 26 + 97);
+                }
+            }
+ 
+                else
+                {
+                    cout << message[j];
+                }
+
+
+        }
+
+        cout << "\n";
+
     }
 
-    cout << "\n";
 
     }
-   
-
-
-
-
-
-  
-  }
 }
